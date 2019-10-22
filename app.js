4f633b851e0617eba0311ba2e4d1f1b275839121
motomotoyoshi <motomoto.git@gmail.com>
2019-10-22 04:57:58 +0000

    add square.js. create module.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const Square = require('./square.js');
+const mySquare = new Square();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000.'));
+app.listen(3000, () => console.log('Example app listening on port 3000.' + mySquare.area(4)));
---
+++ square.js
@@ -0,0 +1,13 @@
+module.exports = class Square {
+
+	constructor(){
+	}
+
+	area(width) { 
+		return width * width;
+	}
+
+	perimeter(width) {
+		 return 4 * width;
+	}
+};
