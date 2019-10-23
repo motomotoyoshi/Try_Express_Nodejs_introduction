db71657a1bb16750b45541fa87f59977bc765c93
motomotoyoshi <motomoto.git@gmail.com>
2019-10-24 02:30:49 +0900

    Use static files.

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use('/wiki', wiki);
 const logger = require('morgan');
 app.use(logger('dev'));
 
+app.use('/media', express.static('public'));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000.' + mySquare.area(4)));
