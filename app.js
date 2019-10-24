2d2ace34f8a3ada74f0032a4194e64d87573e38b
motomotoyoshi <motomoto.git@gmail.com>
2019-10-24 02:46:25 +0000

    Use method POST

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const app = express();
 const myLogger = require('./myLogger.js');
 app.use(myLogger);
 
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+app.engine('ejs', ejs.renderFile);
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 const Square = require('./square.js');
 const mySquare = new Square();
 
@@ -15,6 +22,19 @@ app.use(logger('dev'));
 
 app.use('/media', express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+	console.log('---GET Request---');
+	console.log('Name is ' + req.query.name);
+	console.log('Age is ' + req.query.age);
+	res.render('template.ejs', {})
+});
+
+
+app.post('/', (req, res) => {
+	console.log('---POST Request---');
+	console.log('Name is ' + req.body.name);
+	console.log('Age is ' + req.body.age);
+	res.render('template.ejs', {})
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000.' + mySquare.area(4)));
