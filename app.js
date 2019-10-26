bb89241f47f4e815fb2446c58bf9ac5ea7052923
motomotoyoshi <motomoto.git@gmail.com>
2019-10-26 06:06:14 +0000

    Use session

--- app.js
+++ app.js
@@ -1,43 +1,27 @@
 const express = require('express');
-const app = express();
-
-const cookieParser = require('cookie-parser');
-app.use(cookieParser());
-
 const myLogger = require('./myLogger.js');
-app.use(myLogger);
-
 const ejs = require('ejs');
-const bodyParser = require('body-parser');
+const logger = require('morgan');
+const app = express();
+const session = require('express-session');
+
 app.engine('ejs', ejs.renderFile);
-app.use(bodyParser.urlencoded({
-	extended: true
+app.use(myLogger);
+app.use(logger('dev'));
+app.use(session({
+	secret: 'hoge',
+	resave: true,
+	saveUninitialized: true,
 }));
 
-const Square = require('./square.js');
-const mySquare = new Square();
-
-const wiki = require('./wiki.js');
-app.use('/wiki', wiki);
-
-const logger = require('morgan');
-app.use(logger('dev'));
-
-app.use('/media', express.static('public'));
-
 app.get('/', (req, res) => {
-	let cnt = req.cookies.cnt == undefined ? 0: req.cookies.cnt;
+	let cnt = req.session.cnt == undefined ? 0: req.session.cnt;
 	cnt++;
-	res.cookie('cnt', cnt, {maxAge: 5000});
+	req.session.cnt = cnt;
 
 	res.render('template.ejs', {
 		cnt: cnt
 	});
 });
-
-// app.use((err, req, res, next) => {
-//     console.error(err.static);
-//     res.status(500).send('Something broke!');
-// })
 
 app.listen(3000, () => console.log('Example app listening on port 3000.'));
